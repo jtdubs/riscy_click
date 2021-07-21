72b793e78d8eb4836b532434bcb680903b447118
Justin Dubs <jtdubs@gmail.com>
2021-07-21 18:51:56 -0400

    IF stage works with arbitrary fixed memory delay

--- riscy_click.srcs/sources_1/new/cpu_if.sv
+++ riscy_click.srcs/sources_1/new/cpu_if.sv
@@ -10,6 +10,9 @@
 module cpu_if
     // Import Constants
     import consts::*;
+    #(
+        parameter STALL_CYCLES = 1
+    )
     (
         // cpu signals
         input       logic       clk,            // clock
@@ -35,21 +38,48 @@ module cpu_if
 /// Variables
 ///
 
-logic stall_next; // is the next cycle a stall
-word  pc_next;    // what's the next cycle's program counter 
+localparam integer COUNTER_SIZE = $clog2(STALL_CYCLES);
+
+logic [COUNTER_SIZE:0] stall_ctr;
+logic [COUNTER_SIZE:0] stall_ctr_next;
+logic stall_condition;
+
+word  [STALL_CYCLES:0] pc_fifo;
+word pc_next;
 
 
 ///
 /// Latch outputs
 ///
 
+assign pc = pc_fifo[0];
+
 always_ff @(posedge clk) begin
-    pc    <= pc_next;
-    stall <= stall_next;
+    stall_ctr <= stall_ctr_next;
+    stall     <= (stall_ctr_next > 0);
+    pc_fifo[STALL_CYCLES] <= pc_next;
+end
+
+always_comb begin
+    if (stall_condition) begin
+        stall_ctr_next <= STALL_CYCLES;
+    end else if (stall_ctr_next > 0) begin
+        stall_ctr_next <= stall_ctr - 1;
+    end else begin
+        stall_ctr_next <= 0;
+    end
 end
 
 // ibus data is already registered, so pass it through
 assign ir = ibus_data;
+
+genvar i;
+generate for (i=0; i<STALL_CYCLES; i++)
+    always_ff @(posedge clk) begin
+        pc_fifo[i] <= pc_fifo[i+1];
+    end 
+endgenerate
+
 
 
 ///
@@ -60,22 +90,19 @@ assign ir = ibus_data;
 assign ibus_addr = pc_next;
 
 // Next cycle will stall if reset, halt or jump
-assign stall_next = reset | halt | ex_jmp_valid;
+assign stall_condition = reset | halt | ex_jmp_valid;
 
 // Determine next program counter
 always_comb begin
     if (reset) begin
         // zero if reset
         pc_next <= 0;
-    end else if (stall) begin
-        // no change if in stall cycle
-        pc_next <= pc_next;
     end else if (ex_jmp_valid) begin
         // respect jumps from execute stage
         pc_next <= ex_jmp;
     end else begin
         // otherwise keep advancing
-        pc_next <= pc + 4;
+        pc_next <= pc_fifo[STALL_CYCLES] + 4;
     end
 end
 
