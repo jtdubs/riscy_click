fc90fe95d50f514ec19a2c2942fad7c68bfdc4d8
Justin Dubs <jtdubs@gmail.com>
2021-07-24 16:41:07 -0400

    port skid buffer to systemverilog

--- riscy_click.srcs/sources_1/new/skid_buffer.sv
+++ riscy_click.srcs/sources_1/new/skid_buffer.sv
@@ -1,152 +1,114 @@
 `timescale 1ns / 1ps
 
 module skid_buffer
-#(
-    parameter WORD_WIDTH = 0
-)
-(
-    input   wire logic                       clk,
-    input   wire logic                       reset,
-
-    input   wire logic                       input_valid,
-    output  wire logic                       input_ready,
-    input   wire logic   [WORD_WIDTH-1:0]    input_data,
-
-    output  wire logic                       output_valid,
-    input   wire logic                       output_ready,
-    output  wire logic   [WORD_WIDTH-1:0]    output_data
-);
-
-    localparam WORD_ZERO = {WORD_WIDTH{1'b0}};
-    
-    reg                     data_buffer_wren = 1'b0; // EMPTY at start, so don't load.
-    wire [WORD_WIDTH-1:0]   data_buffer_out;
-
-    register
     #(
-        .WORD_WIDTH     (WORD_WIDTH),
-        .RESET_VALUE    (WORD_ZERO)
+        parameter WORD_WIDTH = 0
     )
-    data_buffer_reg
     (
-        .clk          (clk),
-        .write_enable   (data_buffer_wren),
-        .reset          (reset),
-        .data_in        (input_data),
-        .data_out       (data_buffer_out)
+        input  wire logic                  clk,
+        input  wire logic                  reset,
+    
+        input  wire logic                  input_valid,
+        output      logic                  input_ready,
+        input  wire logic [WORD_WIDTH-1:0] input_data,
+    
+        output      logic                  output_valid,
+        input  wire logic                  output_ready,
+        output      logic [WORD_WIDTH-1:0] output_data
     );
 
-    reg                     data_out_wren       = 1'b1; // EMPTY at start, so accept data.
-    reg                     use_buffered_data   = 1'b0;
-    reg [WORD_WIDTH-1:0]    selected_data       = WORD_ZERO;
+localparam WORD_ZERO = {WORD_WIDTH{1'b0}};
 
-    always @(*) begin
-        selected_data = (use_buffered_data == 1'b1) ? data_buffer_out : input_data;
+//
+// Buffer Register
+//
+
+logic                  data_buffer_wren;
+logic [WORD_WIDTH-1:0] data_buffer;
+
+always_ff @(posedge clk) begin
+    if (reset) begin
+        data_buffer <= WORD_ZERO;
+    end else if (data_buffer_wren) begin
+        data_buffer <= input_data;
     end
+end
 
-    register
-    #(
-        .WORD_WIDTH     (WORD_WIDTH),
-        .RESET_VALUE    (WORD_ZERO)
-    )
-    data_out_reg
-    (
-        .clk          (clk),
-        .write_enable   (data_out_wren),
-        .reset          (reset),
-        .data_in        (selected_data),
-        .data_out       (output_data)
-    );
-    
-       localparam STATE_BITS = 2;
+//
+// Output Register
+//
 
-    localparam [STATE_BITS-1:0] EMPTY = 'd0; // Output and buffer registers empty
-    localparam [STATE_BITS-1:0] BUSY  = 'd1; // Output register holds data
-    localparam [STATE_BITS-1:0] FULL  = 'd2; // Both output and buffer registers hold data
-    // There is no case where only the buffer register would hold data.
+logic data_out_wren;
+logic use_buffered_data;
 
-    // No handling of erroneous and unreachable state 3.
-    // We could check and raise an error flag.
-
-    wire [STATE_BITS-1:0] state;
-    reg  [STATE_BITS-1:0] state_next = EMPTY;
-    
-    register
-    #(
-        .WORD_WIDTH     (1),
-        .RESET_VALUE    (1'b1) // EMPTY at start, so accept data
-    )
-    input_ready_reg
-    (
-        .clk          (clk),
-        .write_enable   (1'b1),
-        .reset          (reset),
-        .data_in        (state_next != FULL),
-        .data_out       (input_ready)
-    );
-    
-    register
-    #(
-        .WORD_WIDTH     (1),
-        .RESET_VALUE    (1'b0)
-    )
-    output_valid_reg
-    (
-        .clk          (clk),
-        .write_enable   (1'b1),
-        .reset          (reset),
-        .data_in        (state_next != EMPTY),
-        .data_out       (output_valid)
-    );
-    
-      reg insert = 1'b0;
-    reg remove = 1'b0;
-
-    always @(*) begin
-        insert = (input_valid  == 1'b1) && (input_ready  == 1'b1);
-        remove = (output_valid == 1'b1) && (output_ready == 1'b1);
+always_ff @(posedge clk) begin
+    if (reset) begin
+        output_data <= WORD_ZERO;
+    end else if (data_out_wren) begin
+        output_data <= (use_buffered_data == 1'b1) ? data_buffer : input_data;
     end
-    
-    reg load    = 1'b0; // Empty datapath inserts data into output register.
-    reg flow    = 1'b0; // New inserted data into output register as the old data is removed.
-    reg fill    = 1'b0; // New inserted data into buffer register. Data not removed from output register.
-    reg flush   = 1'b0; // Move data from buffer register into output register. Remove old data. No new data inserted.
-    reg unload  = 1'b0; // Remove data from output register, leaving the datapath empty.
+end
 
-    always @(*) begin
-        load    = (state == EMPTY) && (insert == 1'b1) && (remove == 1'b0);
-        flow    = (state == BUSY)  && (insert == 1'b1) && (remove == 1'b1);
-        fill    = (state == BUSY)  && (insert == 1'b1) && (remove == 1'b0);
-        flush   = (state == FULL)  && (insert == 1'b0) && (remove == 1'b1);
-        unload  = (state == BUSY)  && (insert == 1'b0) && (remove == 1'b1);
-    end
-    
-       always @(*) begin
-        state_next = (load   == 1'b1) ? BUSY  : state;
-        state_next = (flow   == 1'b1) ? BUSY  : state_next;
-        state_next = (fill   == 1'b1) ? FULL  : state_next;
-        state_next = (flush  == 1'b1) ? BUSY  : state_next;
-        state_next = (unload == 1'b1) ? EMPTY : state_next;
-    end
 
-    register
-    #(
-        .WORD_WIDTH     (STATE_BITS),
-        .RESET_VALUE    (EMPTY)         // Initial state
-    )
-    state_reg
-    (
-        .clk          (clk),
-        .write_enable   (1'b1),
-        .reset          (reset),
-        .data_in        (state_next),
-        .data_out       (state)
-    );
-    
-      always @(*) begin
-        data_out_wren     = (load  == 1'b1) || (flow == 1'b1) || (flush == 1'b1);
-        data_buffer_wren  = (fill  == 1'b1);
-        use_buffered_data = (flush == 1'b1);
+//
+// FSM
+//
+
+typedef enum {
+    EMPTY = 2'b00,
+    BUSY  = 2'b01,
+    FULL  = 2'b10
+} sb_state;
+
+sb_state state, state_next; 
+
+// Can input as long as state isn't going to be FULL
+always_ff @(posedge clk) begin
+    if (reset) begin
+        input_ready <= 1'b1;
+    end else begin
+        input_ready <= (state_next != FULL);
     end
+end 
+
+// Can output as long as state isn't going to be EMPTY
+always_ff @(posedge clk) begin
+    if (reset) begin
+        output_valid <= 1'b0;
+    end else begin
+        output_valid <= (state_next != EMPTY);
+    end
+end
+
+logic insert, remove;
+assign insert = (input_valid  == 1'b1) && (input_ready  == 1'b1);
+assign remove = (output_valid == 1'b1) && (output_ready == 1'b1);
+
+logic load, flow, fill, flush, unload;
+assign load    = (state == EMPTY) && (insert == 1'b1) && (remove == 1'b0); // Empty datapath inserts data into output register.
+assign flow    = (state == BUSY)  && (insert == 1'b1) && (remove == 1'b1); // New inserted data into output register as the old data is removed.
+assign fill    = (state == BUSY)  && (insert == 1'b1) && (remove == 1'b0); // New inserted data into buffer register. Data not removed from output register.
+assign flush   = (state == FULL)  && (insert == 1'b0) && (remove == 1'b1); // Move data from buffer register into output register. Remove old data. No new data inserted.
+assign unload  = (state == BUSY)  && (insert == 1'b0) && (remove == 1'b1); // Remove data from output register, leaving the datapath empty.
+
+always_comb begin
+    state_next = (load   == 1'b1) ? BUSY  : state;
+    state_next = (flow   == 1'b1) ? BUSY  : state_next;
+    state_next = (fill   == 1'b1) ? FULL  : state_next;
+    state_next = (flush  == 1'b1) ? BUSY  : state_next;
+    state_next = (unload == 1'b1) ? EMPTY : state_next;
+end
+
+always_ff @(posedge clk) begin
+    if (reset) begin
+        state <= EMPTY;
+    end else begin
+        state <= state_next;
+    end
+end
+
+assign data_out_wren     = load | flow | flush;
+assign data_buffer_wren  = fill;
+assign use_buffered_data = flush;
 
 endmodule
