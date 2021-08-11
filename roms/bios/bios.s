297cbb56b47b40b369bc1898baa5aa6eab14dc8d
Justin Dubs <jtdubs@gmail.com>
2021-08-11 00:55:29 +0000

    more bios cleanup

--- roms/bios/bios.s
+++ roms/bios/bios.s
@@ -1,72 +1,57 @@
+.file "bios.s"
+
+.equ BIOS_BASE, 0x00000000
+.equ RAM_BASE,  0x10000000
+.equ VRAM_BASE, 0x20000000
+.equ DISPLAY,   0xFF000000
+.equ SWITCH,    0xFF000004
+
 .text
 .align 4
 .globl  _start
 
 _start:
     # set up constants
-    lw x14, SWITCH
-    lw x15, DISPLAY
-    lw x4,  VRAM_BASE
-    lw x5,  RAM_BASE
-    addi x6, x0, 80
-    addi x7, x0, 30
+    li s1, SWITCH
+    li s2, DISPLAY
+    li s3, VRAM_BASE
+    li s4, RAM_BASE
+    li s5, 80
+    li s6, 30
 
     # update display from switch
-    lw x16, 0(x14)
-    sw x16, 0(x15)
+    lw t0, 0(s1)
+    sw t0, 0(s2)
 
-    addi x10, x0, 0 # y=0
-    addi x11, x0, 0 # x=0
+    li s10, 0 # y=0
+    li s11, 0 # x=0
 
 vram_loop:
     # char = (y * 80) + x
-    slli x12, x10, 6
-    slli x8, x10, 4
-    add x12, x12, x8
-    add x12, x12, x11
+    slli t1, s10, 6
+    slli t3, s10, 4
+    add t1, t1, t3
+    add t1, t1, s11
 
     # addr = (y << 7) + x
-    slli x13, x10, 7
-    add x13, x13, x11
-    add x13, x13, x4
+    slli t2, s10, 7
+    add t2, t2, s11
+    add t2, t2, s3
 
     # VRAM[addr] = char
-    sb x12, (x13)
+    sb t1, (t2)
 
     # horizontal loop
-    addi x11, x11, 1
-    bne  x11, x6, vram_loop
+    addi s11, s11, 1
+    bne  s11, s5, vram_loop
 
     # vertial loop
-    addi x11, x0, 0
-    addi x10, x10, 1
-    bne x10, x7, vram_loop
+    li s11, 0
+    addi s10, s10, 1
+    bne s10, s6, vram_loop
 
 seg_loop:
     # update display from switch
-    lw x16, 0(x14)
-    sw x16, 0(x15)
-    jal x0, seg_loop
-
-.section .rodata
-.align 4
-
-.globl BIOS_BASE
-BIOS_BASE:
-.word 0x00000000
-
-.globl RAM_BASE
-RAM_BASE:
-.word 0x10000000
-
-.globl VRAM_BASE
-VRAM_BASE:
-.word 0x20000000
-
-.globl DISPLAY
-DISPLAY:
-.word 0xFF000000
-
-.globl SWITCH
-SWITCH:
-.word 0xFF000004
+    lw t0, 0(s1)
+    sw t0, 0(s2)
+    j seg_loop
