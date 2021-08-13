c9255143ff8c9d03c25730c89851ee7b0b979365
Justin Dubs <jtdubs@gmail.com>
2021-08-13 13:42:20 +0000

    update bios

--- roms/bios/bios.c
+++ roms/bios/bios.c
@@ -2,19 +2,19 @@
 #include "type_defs.h"
 #include "vk_defs.h"
 
-static uint8_t x=10, y=10;
+// static uint8_t x, y;
 
 int main() {
     // zero out the framebuffer
     for (uint8_t y=0; y<30; y++)
         for (uint8_t x=0; x<80; x++)
-            PTR_FRAMEBUFFER[y << 7 | x] = 0;
+            PTR_FRAMEBUFFER[y << 7 | x] = 'X';
 
     // main loop
-    x = 0;
-    y = 0;
+    uint8_t x = 0;
+    uint8_t y = 0;
     for (;;) {
-        PTR_FRAMEBUFFER[y << 7 | x] = 'X';
+        PTR_FRAMEBUFFER[(y << 7) | x] = ' ';
 
         uint32_t c = *PTR_KEYBOARD;
 
@@ -26,14 +26,14 @@ int main() {
         *PTR_DISPLAY = c;
 
         // skip breaks
-        if ((c & 0x100) == 0)
+        if ((c & 0x100) == 0x100)
             continue;
 
         switch (c) {
         case KEY_UP:    y--; break;
         case KEY_DOWN:  y++; break;
-        case KEY_LEFT:  x++; break;
-        case KEY_RIGHT: x--; break;
+        case KEY_LEFT:  x--; break;
+        case KEY_RIGHT: x++; break;
         }
     }
 }
--- roms/bios/mmap_defs.h
+++ roms/bios/mmap_defs.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "type_defs.h"
+
 #define REGION_BIOS        0x00000000
 #define REGION_RAM         0x10000000
 #define REGION_FRAMEBUFFER 0x20000000
@@ -7,9 +9,9 @@
 #define REGION_SWITCH      0xFF000004
 #define REGION_KEYBOARD    0xFF000008
 
-unsigned long * const PTR_BIOS        = (unsigned long *)REGION_BIOS;
-unsigned long * const PTR_RAM         = (unsigned long *)REGION_RAM;
-unsigned long * const PTR_FRAMEBUFFER = (unsigned long *)REGION_FRAMEBUFFER;
-unsigned long * const PTR_DISPLAY     = (unsigned long *)REGION_DISPLAY;
-unsigned long * const PTR_SWITCH      = (unsigned long *)REGION_SWITCH;
-unsigned long * const PTR_KEYBOARD    = (unsigned long *)REGION_KEYBOARD;
+volatile uint32_t * const PTR_BIOS        = (volatile uint32_t * const)REGION_BIOS;
+volatile uint32_t * const PTR_RAM         = (volatile uint32_t * const)REGION_RAM;
+volatile uint8_t  * const PTR_FRAMEBUFFER = (volatile uint8_t  * const)REGION_FRAMEBUFFER;
+volatile uint32_t * const PTR_DISPLAY     = (volatile uint32_t * const)REGION_DISPLAY;
+volatile uint32_t * const PTR_SWITCH      = (volatile uint32_t * const)REGION_SWITCH;
+volatile uint32_t * const PTR_KEYBOARD    = (volatile uint32_t * const)REGION_KEYBOARD;
