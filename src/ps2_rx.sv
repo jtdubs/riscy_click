5be8ba0f7546426704e04b21c4182bdde8d8dcd3
Justin Dubs <jtdubs@gmail.com>
2021-08-14 14:35:35 +0000

    ps2 rx cleanup

--- src/ps2_rx.sv
+++ src/ps2_rx.sv
@@ -1,12 +1,8 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-// Was:
-// - 54 cells
-// - 63 nets
-
 ///
-/// Keyboard Controller
+/// PS/2 Receiver
 ///
 
 module ps2_rx
@@ -61,7 +57,7 @@ end
 //   ^  ^                            |              |
 //   |  |-----------fail-------------|              |
 //   |                                              |
-//   |-----------------key,abort--------------------|
+//   |----------------packet,abort------------------|
 
 typedef enum logic [1:0] {
     IDLE   = 2'b00,
@@ -77,24 +73,24 @@ byte_t      data_r;
 logic       parity_r, parity_w;
 
 // transitions
-logic idle_w;
+logic reject_w;
 logic start_w;
 logic recv_w;
 logic check_w;
 logic pass_w;
 logic fail_w;
-logic key_w;
+logic packet_w;
 logic abort_w;
 
 always_comb begin
-    idle_w  = falling_edge_w && (ps2_state_r == IDLE)   && (ps2_data_r == 1'b1);
-    start_w = falling_edge_w && (ps2_state_r == IDLE)   && (ps2_data_r == 1'b0);
-    recv_w  = falling_edge_w && (ps2_state_r == DATA)   && (bits_r <  4'd7);
-    check_w = falling_edge_w && (ps2_state_r == DATA)   && (bits_r == 4'd7);
-    pass_w  = falling_edge_w && (ps2_state_r == PARITY) &&  parity_w;
-    fail_w  = falling_edge_w && (ps2_state_r == PARITY) && !parity_w;
-    key_w   = falling_edge_w && (ps2_state_r == STOP)   &&  ps2_data_r;
-    abort_w = falling_edge_w && (ps2_state_r == STOP)   && !ps2_data_r;
+    reject_w = falling_edge_w && (ps2_state_r == IDLE)   && (ps2_data_r == 1'b1);
+    start_w  = falling_edge_w && (ps2_state_r == IDLE)   && (ps2_data_r == 1'b0);
+    recv_w   = falling_edge_w && (ps2_state_r == DATA)   && (bits_r <  4'd7);
+    check_w  = falling_edge_w && (ps2_state_r == DATA)   && (bits_r == 4'd7);
+    pass_w   = falling_edge_w && (ps2_state_r == PARITY) &&  parity_w;
+    fail_w   = falling_edge_w && (ps2_state_r == PARITY) && !parity_w;
+    packet_w = falling_edge_w && (ps2_state_r == STOP)   &&  ps2_data_r;
+    abort_w  = falling_edge_w && (ps2_state_r == STOP)   && !ps2_data_r;
 end
 
 // keep track of parity
@@ -102,7 +98,7 @@ always_comb parity_w = (parity_r + ps2_data_r);
 
 // determine next state
 always_comb begin
-    if (idle_w || fail_w || key_w || abort_w)
+    if (reject_w || fail_w || packet_w || abort_w)
         ps2_state_w = IDLE;
     else if (start_w || recv_w)
         ps2_state_w = DATA;
@@ -125,7 +121,7 @@ end
 // take transition actions
 always_ff @(posedge clk_i) begin
     data_o   <= data_r;
-    valid_o  <= key_w;
+    valid_o  <= packet_w;
 
     if (start_w) begin
         bits_r   <= 4'b0;
